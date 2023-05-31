2080ff514fbb153ce76e311de3d1d32b574ccb41
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-05-31 16:28:00 +0600

    delete and patch method implement

--- src/pages/Checkings/CheckBookingRow.jsx
+++ src/pages/Checkings/CheckBookingRow.jsx
@@ -1,19 +1,20 @@
 
-const CheckBookingRow = ({ checking }) => {
-    const { img, date, service, price } = checking;
+const CheckBookingRow = ({ checking, handleDelete, handleBookingConfirm }) => {
+    const { _id, img, date, service, price, status } = checking;
+
+
     return (
         <tr>
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button onClick={() => handleDelete(_id)} className="btn btn-sm btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </th>
             <td>
                 <div className="avatar">
                     <div className="rounded w-24 h-24">
-                  { img && <img src={img} alt="avatar tailwind CSS Component" />}
+                        {img && <img src={img} alt="avatar tailwind CSS Component" />}
                     </div>
-
                 </div>
 
             </td>
@@ -23,7 +24,9 @@ const CheckBookingRow = ({ checking }) => {
             <td>{date}</td>
             <td>$ {price}</td>
             <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                {status === 'confirm' ? <span className="font-bold text-primary">Confirmed</span> :
+
+                    <button onClick={() => handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Please Confirm</button>}
             </th>
         </tr>
     );
--- src/pages/Checkings/Checkings.jsx
+++ src/pages/Checkings/Checkings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import CheckBookingRow from "./CheckBookingRow";
+import Swal from "sweetalert2";
 
 const Checkings = () => {
 
@@ -12,7 +13,53 @@ const Checkings = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setCheckings(data))
-    }, [])
+    }, [url])
+
+
+    const handleDelete = id => {
+        const proceed = confirm('are you sure you want to delete');
+        if (proceed) {
+            fetch(`http://localhost:5000/checkings/${id}`, {
+                method: "DELETE",
+            })
+                .then(res => res.json())
+
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert("Deleted successful!")
+
+                        const remaining = checkings.filter(checkBooking => checkBooking._id !== id);
+                        setCheckings(remaining);
+
+                    }
+                })
+        }
+
+    }
+
+    const handleBookingConfirm = id => {
+        fetch(`http://localhost:5000/checkings/${id}`, {
+            method: "PATCH",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: "confirm" })
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    // update state
+                    const remaining = checkings.filter(checkBooking => checkBooking._id !== id);
+                    const updated = checkings.find(checkBooking => checkBooking._id === id);
+                    updated.status = "confirm";
+                    const newCheckBookings =  [updated, ...remaining];
+                    setCheckings(newCheckBookings)
+                }
+            })
+    }
 
     return (
         <div>
@@ -27,20 +74,22 @@ const Checkings = () => {
                                     <input type="checkbox" className="checkbox" />
                                 </label>
                             </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Image</th>
+                            <th>Service</th>
                             <th>Date</th>
+                            <th>price</th>
                             <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             checkings.map(checking => <CheckBookingRow
-                            key={checking._id}
-                            checking={checking}
+                                key={checking._id}
+                                checking={checking}
+                                handleDelete={handleDelete}
+                                handleBookingConfirm={handleBookingConfirm}
                             ></CheckBookingRow>)
-                            
+
                         }
                     </tbody>
 
